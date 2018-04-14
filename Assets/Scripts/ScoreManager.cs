88f21a4d922342f39bb6d1a27efa68e3e63765f5
Jacob Kaae Olesen <jko@moviestarplanet.com>
2018-04-14 14:54:07 +0200

    Added score prefab

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	private int _score;
+	public Text scoreText;
+	private string _prefix;
+
+	// Use this for initialization
+	void Start () {
+		_score = 0;
+		_prefix = "Score: ";
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		scoreText.text = _prefix + _score.ToString();	
+	}
+
+	public void UpdateScore(int value)
+	{
+		_score += value;
+	}
+
+	public int GetScore()
+	{
+		return _score;
+	}
+}
