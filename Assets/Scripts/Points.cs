c767bbcd37bcf067b0979e45f8f09637a20e3344
Andrew Borg Cardona <abcfantasy@gmail.com>
2018-04-15 11:52:55 +0200

    Adding score

---
+++ Assets/Scripts/Points.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Points : MonoBehaviour {
+
+    public float TTLSeconds;
+    public float Speed;
+    private float timeLived = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if ( timeLived >= TTLSeconds)
+        {
+            // destroy
+            GameObject.Destroy(this.gameObject);
+        }
+
+        timeLived += Time.deltaTime;
+        this.transform.position = new Vector3(this.transform.position.x, transform.position.y + (Speed * Time.deltaTime), transform.position.z);
+	}
+}
